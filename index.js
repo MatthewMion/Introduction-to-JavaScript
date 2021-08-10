f0a883cbd8e903f440f6caf19a9a9a3380b029dd
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-08-10 15:43:14 -0400

    Completed Task 5

--- index.js
+++ index.js
@@ -217,8 +217,10 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
+function miles(km){
   /*add your code here*/
+  return km * 0.621371;
+  
 }
 
 
@@ -231,8 +233,10 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
+function feet(cm){
   /*add your code here*/
+  return cm/30.48;
+ 
 }
 
 
